1c4fef7381db83627fa9423d65e46c11967fa5cb
ладос <vldkryukov@gmail.com>
2025-08-29 19:47:13 +0500

    Fix CSP issues: disable sourcemaps, add Content Security Policy meta tag

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false, // Отключаем source maps для решения CSP проблем
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
